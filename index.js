1ceff3886361952c4770394584f1cb5a7db4fa9a
uziel78 <rune.husa78@gmail.com>
2025-03-25 14:36:34 +0100

    basic object rendering

--- index.js
+++ index.js
@@ -18,3 +18,31 @@ const renderer = new THREE.WebGLRenderer();
 console.log(scene);
 console.log(camera);
 console.log(renderer);
+
+// * Set the size of the renderer (full screen)
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// * append the renderer to the body element
+document.body.appendChild(renderer.domElement);
+
+// * Create a box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+// * Create a material
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+console.log(boxGeometry);
+console.log(material);
+
+// * Create a mesh using the box geometry and material
+const mesh = new THREE.Mesh(boxGeometry, material);
+
+console.log(mesh);
+
+// * Add the mesh to the scene
+scene.add(mesh);
+
+// * Set the camera position
+camera.position.z = 5;
+
+// * call the render function
+renderer.render(scene, camera);
